f57f6bf32216345d1dc540ca4f017fc3a308bcd8
Sean Dou <douguohui@gmail.com>
2016-03-29 17:53:00 +0800

    expose view methods to controller, which can be invoked in action

--- src/application/core/controller.js
+++ src/application/core/controller.js
@@ -21,4 +21,30 @@ export default class Controller {
 
   after() {
   }
+
+  /**
+   * Render html with view file and data
+   * @param  {String} view View file path
+   * @param  {Object} data
+   */
+  render(view, data = {}) {
+    this.ctx.render(view, data);
+  }
+
+  /**
+   * Render html
+   * @param  {String} html
+   */
+  text(html) {
+    this.ctx.text(html);
+  }
+
+  /**
+   * Render json
+   * @param  {Objecdt} data
+   */
+  json(data) {
+    this.ctx.json(data);
+  }
+
 }
