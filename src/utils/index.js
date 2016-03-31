6556082e2d556453c387410f240a1c988e42df70
Sean Dou <douguohui@gmail.com>
2016-03-31 14:50:33 +0800

    more utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
 import lodash from 'lodash';
-
+import async from 'async';
+import validator from 'validator';
+import moment from 'moment';
 import common from './common';
 import requireAll from './requireAll';
 
 lodash.mixin(common);
 
 lodash.mixin({
+  async: async,
+  moment: moment,
+  validator: validator,
   requireAll: requireAll
 });
 
