fd6bb24df4344097c9cdb07197f7e2eab6893b5f
Sean Dou <douguohui@gmail.com>
2016-04-01 11:08:43 +0800

    set global paths

--- src/application/exposeGlobals.js
+++ src/application/exposeGlobals.js
@@ -5,5 +5,9 @@ import util from './../utils';
 export default function exposeGlobals(app) {
   global['_'] = util;
 
+  app.set('rootPath', app.rootPath);
+  app.set('frameworkPath', app.frameworkPath);
+  app.set('npmBinPath', app.npmBinPath);
+
   app.debug('expose globals');
 };
