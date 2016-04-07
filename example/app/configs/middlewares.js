430f74710ebe88ae547a2cd3814c01bfffd6c597
Sean Dou <douguohui@gmail.com>
2016-04-07 12:57:07 +0800

    middleware config

---
+++ example/app/configs/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = {
+  
+  order: [
+    'logger',
+    'base',
+    'redis',
+    'session',
+    'orm',
+    'view',
+    'static',
+    'filter',
+    'csrf',
+    'asset',
+    'passport',  
+  ],
+
+  custom: {}
+
+};
