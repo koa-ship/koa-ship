39e7bf87d9de27ff1ee5cd797a7e34ca73f1cce1
Sean Dou <douguohui@gmail.com>
2016-04-18 10:25:03 +0800

    load lib from local lib directory

--- src/application/exposeGlobals.js
+++ src/application/exposeGlobals.js
@@ -1,10 +1,28 @@
 'use strict';
 
+import path from 'path';
 import util from './../utils';
 
+/**
+ * Load liberary from rootpath/path/to/lib
+ * @param  {Object} app koa-ship Application handler
+ * @return {Object}     loaded lib
+ */
+var loadLib = function(app) {
+  return function(libPath) {
+    return require(path.join(app.rootPath, 'lib', libPath));
+  };
+};
+
+/**
+ * Expose global variables to application or controller actions
+ * @param  {Object} app koa-ship Application handler
+ */
 export default function exposeGlobals(app) {
   global['_'] = util;
+  global['requireLib'] = loadLib(app);
 
+  // set consts which can be invoked in action.
   app.set('rootPath', app.rootPath);
   app.set('dataPath', app.dataPath);
   app.set('tmpPath', app.tmpPath);
